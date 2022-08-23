8ebe061d3afb88c87282bb8e35e1ae8c2376d9d0
cobra <bakoibrahim258@gmail.com>
2022-08-23 15:55:09 +0000

    ajout de la condition de DEBUG dans les urls

--- backend/urls.py
+++ backend/urls.py
@@ -13,9 +13,13 @@ urlpatterns = [
     path('api/store/', include('store.urls')),
 ]
 
-
-urlpatterns += [
-    path('__debug__/', include('debug_toolbar.urls')),
-]
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if(settings.DEBUG == True):
+
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
+
