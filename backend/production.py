20a90656206f025ea7c58c31e096f667435984c5
cobra <bakoibrahim258@gmail.com>
2022-08-23 17:00:59 +0000

    configuration de la database prostgre sql pour le server de developement

--- backend/production.py
+++ backend/production.py
@@ -1,11 +1,16 @@
 from .settings import *
 
+import dj_database_url
+
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 
 
 SECRET_KEY = 'k3$m2^e%@$f!nb@=i2qzmrybe%oag!(&3e3o8d(!7@&o8g0=df'
+
+
+DATABASES['default'] = dj_database_url.config()
 
 ALLOWED_HOSTS = [
     'shopbackend258.herokuapp.com',
