48bb10c01c2b91c13458b67418fb1ab1bf6753f6
cobra <bakoibrahim258@gmail.com>
2022-08-23 15:48:31 +0000

    creation du fichier de production production.py

---
+++ backend/production.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+
+SECRET_KEY = 'k3$m2^e%@$f!nb@=i2qzmrybe%oag!(&3e3o8d(!7@&o8g0=df'
+
+ALLOWED_HOSTS = [
+    'shopbackend258.herokuapp.com',
+]
